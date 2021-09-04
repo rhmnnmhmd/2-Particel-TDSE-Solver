e367fd12f4a046f72bdeeda8abe8638d386b84ec
Abdul Rahman bin Mohd @ Mohamed <rahman99legacy@gmail.com>
2021-09-04 18:25:36 +0800

    Update (Rahman)TDSE_Solver.py

--- (Rahman)TDSE_Solver.py
+++ (Rahman)TDSE_Solver.py
@@ -23,31 +23,30 @@ from IPython.display import HTML
 #plt.style.use(["science", "notebook", "grid"])
 plt.rc('savefig', dpi=300)
 
-#RUNTIME--------------------------------------------------------------------
-start = time.time()
-
 #POTENTIALS----------------------------------------------------------------
 #soft Gaussian potentials
 def potential():
     V = np.zeros((Nx, Nx))
-    
+    tmp = (dx**2)/(2*alpha**2)
+
     #Gaussian potential
     for m in range(1, Nx-1):
     
-        for n in range(1, Nx-1):
-            V[m, n] = V0*np.exp(-(np.abs(x1[m] - x2[n])**2) / (2*alpha**2))
+      for n in range(1, Nx-1):
+        V[m, n] = V0*np.exp(-tmp*(m-n)*(m-n))
+        #V[m, n] = V0*np.exp(-(np.abs(x1[m] - x2[n])**2) / (2*alpha**2))
     
     #hard square potential
-    # for m in range(0, Nx):
-    
-    #     for n in range(0, Nx):
+    #for m in range(0, Nx):
+      
+      #for n in range(0, Nx):
         
-    #         if np.abs(m-n)*dx <= alpha:
-    #             V[m, n] = V0
+        #if np.abs(m-n)*dx <= alpha:
+          #V[m, n] = V0
             
-    #         else:
-    #             V[m, n] = 0.0
-                
+        #else:
+          #V[m, n] = 0.0
+    
     return V
 
 #INITIAL WAVEFUNCTION-----------------------------------------------------
@@ -58,16 +57,23 @@ def initialize():
     X01 = x01*Nx*dx
     X02 = x02*Nx*dx
 
-    #ww = (k1*k1/(2.0*m1) + k2*k2/(2.0*m2))*0.5*dt
+    ww = (k1*k1/(2.0*m1) + k2*k2/(2.0*m2))*0.5*dt
 
     for m in range(1, Nx-1):
     
-        for n in range(1, Nx-1):
-            A = - ((x1[m] - X01)**2 + (x2[n] - X02)**2) / (4*sigma**2)
-            B = k1*x1[m] + k2*x2[n]
-            RePsi[0, m, n] = np.exp(A) * np.cos(B)
-            ImPsi[0, m, n] = np.exp(A) * np.sin(B)
-    
+      for n in range(1, Nx-1):
+        y = k1*x1[m] - k2*x2[n]
+        y = y - ww
+        a1 = (x1[m]-X01)/sigma
+        a2 = (x2[n]-X02)/sigma
+        a4 = np.exp(-(a1*a1 + a2*a2) / 2)
+        #A = - ((x1[m] - X01)**2 + (x2[n] - X02)**2) / (4*sigma**2)
+        #B = k1*x1[m] + k2*x2[n]
+        RePsi[0, m, n] = a4*np.cos(y)
+        ImPsi[0, m, n] = a4*np.sin(y)
+        #RePsi[0, m, n] = np.exp(A) * np.cos(B)
+        #ImPsi[0, m, n] = np.exp(A) * np.sin(B)
+      
     return RePsi, ImPsi
 
 #SOLVING SE---------------------------------------------------------------
@@ -78,84 +84,119 @@ def solveSE():
     
     emptyArray3 = np.empty((Nt, Nx))
     
+    Rho = np.zeros((Nx, Nx))
+
     for l in range(1, Nt):
     
-        #real part of wavefunction
-        for m in range(1, Nx-1):
+      #real part of wavefunction
+      for m in range(1, Nx-1):
         
-            for n in range(1, Nx-1):
-                a2 = dt*V[m, n]*ImPsi[l-1, m, n] + con2*ImPsi[l-1, m, n]
+        for n in range(1, Nx-1):
+          a2 = dt*V[m, n]*ImPsi[l-1, m, n] + con2*ImPsi[l-1, m, n]
              
-                a1 = dm12*(ImPsi[l-1, m+1, n] + ImPsi[l-1, m-1, n])
-                + dm22*(ImPsi[l-1, m, n+1] + ImPsi[l-1, m, n-1])
+          a1 = dm12*(ImPsi[l-1, m+1, n] + ImPsi[l-1, m-1, n]) + dm22*(ImPsi[l-1, m, n+1] + ImPsi[l-1, m, n-1])
                    
-                RePsi[l, m, n] = RePsi[l-1, m, n] - dtx*a1 + 2.0*a2
-     
-        #initialize single-particle probability
-        rho1 = np.zeros(Nx) 
-        rho2 = np.zeros(Nx)  
-    
-        #sum probabilities
-        SumRho = np.zeros(Nx)
-        
-        p = 1
+          RePsi[l, m, n] = RePsi[l-1, m, n] - dtx*a1 + 2.0*a2
 
-        for m in range(1, Nx-1):
+          Rho[m, n] = RePsi[l-1, m, n]*RePsi[l, m, n] + ImPsi[l-1, m, n]*ImPsi[l-1, m, n]
+     
+      #initialize single-particle probability
+      rho1 = np.zeros(Nx) 
+      rho2 = np.zeros(Nx)  
+      corr = np.zeros(Nx)
+    
+      #sum probabilities
+      SumRho = np.zeros(Nx)
+        
+      Ptot = 0.0
+      Prel = 0.0
+
+      p = 1
+
+      for m in range(1, Nx-1):
+        k = 1
+
+        if p == 3:
+          p = 1
+          
+        for n in range(1, Nx):
+
+          if k == 3:
             k = 1
             
-            if p == 3:
-                p = 1
-    
-            for n in range(1, Nx-1):
-        
-                if k == 3:
-                    k = 1
-                
-                #for particle 1
-                Rho = RePsi[l-1, m, n]*RePsi[l, m, n] 
-                + ImPsi[l-1, m, n]*ImPsi[l-1, m, n]
-                
-                Rho = Rho + symmetry*(RePsi[l-1, m, n]*RePsi[l, n, m] 
-                + ImPsi[l-1][m][n]*ImPsi[l-1][n][m])
-                
-                rho1[m] = rho1[m] + w[k]*Rho
+          Ptot = Ptot + w[k]*w[p]*Rho[m, n]
 
+          k = k + 1
+          
+        p = p + 1
 
-                #for particle 2
-                Rho = RePsi[l-1, n, m]*RePsi[l, n, m] 
-                + ImPsi[l-1, n, m]*ImPsi[l-1, n, m]
-                
-                Rho = Rho + symmetry*(RePsi[l-1, n, m]*RePsi[l, m, n] 
-                + ImPsi[l-1][n][m]*ImPsi[l-1][m][n])
-                
-                rho2[m] = rho2[m] + w[k]*Rho
-        
-                k = k+1
-        
-            SumRho[m] = rho1[m] + rho2[m]
+      for m in range(1, Nx-1):
+
+        for n in range(1, Nx-1):
+          Rho[m, n] = Rho[m, n] / Ptot
+
+      p = 1
+
+      #get rho1, rho2 and Rho
+      for m in range(1, Nx-1):
+        k = 1
+            
+        if p == 3:
+          p = 1
     
-            if np.abs(SumRho[m]) < 1e-20:
-                SumRho[m] = 0.0
-    
-            p = p + 1
-                    
-        emptyArray1[l-1, :] = rho1
+        for n in range(1, Nx-1):
         
-        emptyArray2[l-1, :] = rho2
-        
-        emptyArray3[l-1, :] = SumRho
-        
-        #imaginary part of wave packet is next
-        for m in range(1, Nx-1):
-        
-            for n in range(1, Nx-1):
-                a2 = dt*V[m, n]*RePsi[l, m, n] + con2*RePsi[l, m, n]
+          if k == 3:
+            k = 1
                 
-                a1 = dm12*(RePsi[l, m+1, n]+RePsi[l, m-1, n]) 
-                + dm22*(RePsi[l, m, n+1]+RePsi[l, m, n-1])
-                    
-                ImPsi[l, m, n] = ImPsi[l-1, m, n] + dtx*a1 - 2.0*a2 
+          #for particle 1
+          rho1[m] = rho1[m] + w[k]*Rho[m, n]
+               
+          #for particle 2
+          rho2[m] = rho2[m] + w[k]*Rho[n, m]
         
+          k = k+1
+        
+        SumRho[m] = rho1[m] + rho2[m]
+    
+        if np.abs(SumRho[m]) < 1e-20:
+          SumRho[m] = 0.0
+    
+        p = p + 1
+
+      #correlation stuffs       
+      for m in range(1, Nx-1):
+        n = Nx - 1 - m
+        x = m - n
+        x = np.abs(x)
+        
+        if (rho1[m] != 0.0) and (rho2[n] != 0.0):
+          corr[m] = Rho[m, n] / (rho1[m]*rho2[n])
+
+        if (rho1[n] != 0.0) and (rho2[m] != 0.0):
+          corr[m] = corr[m] + Rho[n, m]/(rho1[n]*rho2[m])
+
+        if corr[m] != 0.0:
+          corr[m] = math.log10(np.abs(corr[m]))
+
+      emptyArray1[l-1, :] = rho1
+        
+      emptyArray2[l-1, :] = rho2
+        
+      emptyArray3[l-1, :] = SumRho
+        
+      #imaginary part of wave packet is next
+      for m in range(1, Nx-1):
+        
+        for n in range(1, Nx-1):
+          a2 = dt*V[m, n]*RePsi[l, m, n] + con2*RePsi[l, m, n]
+                
+          a1 = dm12*(RePsi[l, m+1, n]+RePsi[l, m-1, n]) + dm22*(RePsi[l, m, n+1]+RePsi[l, m, n-1])
+                    
+          ImPsi[l, m, n] = ImPsi[l-1, m, n] + dtx*a1 - 2.0*a2 
+    
+      #energy
+
     return emptyArray1, emptyArray2, emptyArray3
 
 #CONSTANTS-----------------------------------------------------------------
@@ -195,12 +236,12 @@ x01 = 0.25
 x02 = 0.75
 
 #peak potential value
-V0 = -100000.0
+V0 = 100000.0
 
 #potential width
 alpha = 0.062
 
-#Simpson integration weights
+#w's
 w = np.zeros(3)
 w[0] = dx/3
 w[1] = (4/3)*dx
@@ -248,8 +289,37 @@ plot2 = sol[1]
 
 plot3 = sol[2]
 
-#RUNTIME--------------------------------------------------------------------
-#end timer
-time.sleep(1)
-end = time.time()
-print(f"Runtime is {end-start} seconds")
+#PLOTTING OR ANIMATIONS---------------------------------------------------
+fig, ax = plt.subplots(1, 1)
+
+ax.set_xlim(0, 1.5)
+#ax.set_ylim(0, 0.1)
+
+ax.plot(x1, np.abs(plot1[0, :]))
+ax.plot(x1, np.abs(plot2[0, :]))
+
+#PLOTTING OR ANIMATIONS---------------------------------------------------
+# fig, ax = plt.subplots(1, 1)
+
+# ax.set_xlim(0, 1.5)
+# ax.set_ylim(0, 0.1)
+
+# line1, = ax.plot([], [])
+# line2, = ax.plot([], [])
+
+# def init():
+#     line1.set_data([], [])
+#     line2.set_data([], [])
+
+#     return line1, line2,
+
+# def animate(i):
+#     line1.set_data(x1, plot1[i, :])
+#     line2.set_data(x1, plot2[i, :])
+
+#     return line1, line2, 
+
+# #create an animation
+# anim = animation.FuncAnimation(fig, animate, init_func = init, frames = Nt, interval = 50, blit = True)
+
+# anim.save('collission.mp4', fps=24, extra_args=['-vcodec', 'libx264'], dpi = 150)
